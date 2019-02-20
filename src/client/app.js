15ea6f00f6c41abe04dd96d2c89fbd63b20f23e0
Arravind <aravindram024@gmail.com>
2019-02-20 14:38:08 +0530

    Added Styles and Fixed Height

--- src/client/app.js
+++ src/client/app.js
@@ -5,7 +5,11 @@ import Styles from './style.scss';
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello from React!</h1>
+            <div className="content-wrap">
+                <h2>Get Started</h2>
+                <h1>Simple MERN Stack Boilerplate</h1>
+                <a className="github-btn" href="https://github.com/arravind-r/simple-mern-stack" target="_blank">GitHub Repo</a>
+            </div>
     );
     }
 }
--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,7 @@ const db = require('../../.env').mongoURI;
 
 //Connect to MongoDB
 mongoose.connect(db)
-    .then(()=> console.log('MongoDB is now connected'))
+    .then(()=> console.log('MongoDB is successfully connected'))
     .catch(err => console.log(err));
 
 // Defining public path (relative path)
@@ -17,6 +17,6 @@ const publicPath = path.resolve(__dirname, '..', '..', 'public');
 app.use(express.static(publicPath));
 
 // Simply logging to console
-app.listen(3000, () => {
-    console.log(`MERN Boilerplate listening on port 3000`);
+app.listen(8000, () => {
+    console.log(`Express has started running on PORT: 8000`);
 });
